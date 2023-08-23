74e261ca890ccae823d98957d157da8ac93480bb
Kiara Hoheb <121005877+kxtara@users.noreply.github.com>
2023-08-23 19:07:32 -0400

    Added service card component

---
+++ ariels-beauty-app/src/components/ServiceCards/ServicesInfo.js
@@ -0,0 +1,96 @@
+import boxbraid from "../../assets/images/boxbraid.jpg";
+import braidedpony from "../../assets/images/braidedpony.jpg";
+
+const washInfo = [
+  {
+    title: "Wash",
+    image: boxbraid,
+    imageAlt: "lady with box braids",
+    description:
+      "Our in-house expert in braids will perfect your box braids to Pinterest-worthy levels. ",
+    price: "$120",
+    detailedDescription:
+      "Our braided ponytail offers a simple yet elegant style that suits various occasions. For more intricate and elaborate designs, our pricing may vary depending on the level of intricacy and the additional time involved.",
+  },
+  {
+    title: "Braided Ponytail",
+    image: braidedpony,
+    imageAlt: "lady with box braids",
+    description:
+      "Our in-house expert in braids will perfect your box braids to Pinterest-worthy levels. ",
+    price: "$120",
+    detailedDescription:
+      "Our braided ponytail offers a simple yet elegant style that suits various occasions. For more intricate and elaborate designs, our pricing may vary depending on the level of intricacy and the additional time involved.",
+  },
+];
+
+const installInfo = [
+  {
+    title: "Install",
+    image: boxbraid,
+    imageAlt: "lady with box braids",
+    description:
+      "Our in-house expert in braids will perfect your box braids to Pinterest-worthy levels. ",
+    price: "$120",
+    detailedDescription:
+      "Our braided ponytail offers a simple yet elegant style that suits various occasions. For more intricate and elaborate designs, our pricing may vary depending on the level of intricacy and the additional time involved.",
+  },
+  {
+    title: "Braided Ponytail",
+    image: braidedpony,
+    imageAlt: "lady with box braids",
+    description:
+      "Our in-house expert in braids will perfect your box braids to Pinterest-worthy levels. ",
+    price: "$120",
+    detailedDescription:
+      "Our braided ponytail offers a simple yet elegant style that suits various occasions. For more intricate and elaborate designs, our pricing may vary depending on the level of intricacy and the additional time involved.",
+  },
+];
+
+const braidsInfo = [
+  {
+    title: "Braids",
+    image: boxbraid,
+    imageAlt: "lady with box braids",
+    description:
+      "regular description for braids",
+    price: "$120",
+    detailedDescription:
+      "detatiled description for braids",
+  },
+  {
+    title: "Braided Ponytail",
+    image: braidedpony,
+    imageAlt: "lady with braided ponytail",
+    description:
+      "Our in-house expert in braids will perfect your box braids to Pinterest-worthy levels. ",
+    price: "$120",
+    detailedDescription:
+      "Our braided ponytail offers a simple yet elegant style that suits various occasions. For more intricate and elaborate designs, our pricing may vary depending on the level of intricacy and the additional time involved.",
+  },
+];
+
+const ponytailInfo = [
+  {
+    title: "PonyTail",
+    image: boxbraid,
+    imageAlt: "lady with box braids",
+    description:
+      "Our in-house expert in braids will perfect your box braids to Pinterest-worthy levels. ",
+    price: "$120",
+    detailedDescription:
+      "Our braided ponytail offers a simple yet elegant style that suits various occasions. For more intricate and elaborate designs, our pricing may vary depending on the level of intricacy and the additional time involved.",
+  },
+  {
+    title: "Braided Ponytail",
+    image: braidedpony,
+    imageAlt: "lady with box braids",
+    description:
+      "Our in-house expert in braids will perfect your box braids to Pinterest-worthy levels. ",
+    price: "$120",
+    detailedDescription:
+      "Our braided ponytail offers a simple yet elegant style that suits various occasions. For more intricate and elaborate designs, our pricing may vary depending on the level of intricacy and the additional time involved.",
+  },
+];
+
+export { washInfo, installInfo, braidsInfo, ponytailInfo };
