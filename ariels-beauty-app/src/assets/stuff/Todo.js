540e7ac598ee9501b5a44fbcd81619132ad8a913
Kiara Hoheb <121005877+kxtara@users.noreply.github.com>
2023-08-28 22:03:13 -0400

    Added task

--- ariels-beauty-app/src/assets/stuff/Todo.js
+++ ariels-beauty-app/src/assets/stuff/Todo.js
@@ -15,9 +15,13 @@ Todo: 8/17/23 (Completed)
     • New Logo (Checked)
 
 Todo: 8/23/23
-    • //!Create slider
+    • Create slider (Checked)
     • Set up links for slider elements (Checked)
     • Create a button to change back from the book button (Checked)
     • Create array of objects to store info about styles (Checked)
     • Get images and descritions for cards (Checked)
+
+Todo: 8/28/23
+    • Write Process on Services page
+    • Continue Contact page
 */
