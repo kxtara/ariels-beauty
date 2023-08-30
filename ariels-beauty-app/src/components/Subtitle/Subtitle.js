b5bdfe5eefb2c4075c779983ba5ed36729147e50
Kiara Hoheb <121005877+kxtara@users.noreply.github.com>
2023-08-30 02:06:13 -0400

    Update Subtitle.js

--- ariels-beauty-app/src/components/Subtitle/Subtitle.js
+++ ariels-beauty-app/src/components/Subtitle/Subtitle.js
@@ -1,9 +1,9 @@
 import React from "react";
-function Subtitle(props){
+function Subtitle({title}){
     return(
         <div className="flex justify-center items-center gap-4 my-8 mx-0">
             <div className="bg-[#6C705F] w-12 h-[2px]"></div>
-            <h2 className="text-[#4D4D4D] text-center text-[1.6875rem] max-w-full overflow-hidden font-['IM_FELL_French_Canon_SC']">{props.title}</h2>
+            <h2 className="text-[#4D4D4D] text-center text-[1.6875rem] max-w-full overflow-hidden font-['IM_FELL_French_Canon_SC']">{title}</h2>
             <div className="bg-[#6C705F] w-12 h-[2px]"></div>
         </div>
     )
