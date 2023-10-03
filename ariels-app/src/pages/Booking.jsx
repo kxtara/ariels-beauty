f3856e7bdba99ea7f0fc8b76fe045b13c0f18c18
Kiara Hoheb <121005877+kxtara@users.noreply.github.com>
2023-10-03 18:37:29 -0400

    Started Booking page

--- ariels-app/src/pages/Booking.jsx
+++ ariels-app/src/pages/Booking.jsx
@@ -1,10 +1,33 @@
-import React from "react";
-function Booking() {
-    return (
-        <div className="pt-[16rem]">
-            <h1>This is the Booking page</h1>
-        </div>
-    )
-}
+import { useSearchParams } from "react-router-dom";
+import Hero from "../components/Hero";
+export default function Booking() {
+  const [searchParams] = useSearchParams();
 
-export default Booking
+  const { title, price, image, imageAlt } = Object.fromEntries(
+    searchParams.entries()
+  );
+
+  const onChange = (e) => {
+    setDate(date);
+  };
+  return (
+    <>
+      <Hero title="hello" />
+      <div className="bg-[#fffdfd] border-[1px] border-[rgba(30, 30, 30, 43%)] rounded-[0.75rem] w-[24rem] m-auto mt-7 mb-7 flex">
+        <img
+          className="w-[9rem] rounded-tl-[0.3125rem] rounded-bl-[0.3125rem]"
+          src={image}
+          alt={imageAlt}
+        />
+        <div className="ml-3">
+          <h4 className="mt-4 font-bold uppercase">{title}</h4>
+          <h3 className="font-semibold ">{price}</h3>
+          <p>
+            <span className="font-semibold">Terms:</span>
+            blag lah mdidm
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
