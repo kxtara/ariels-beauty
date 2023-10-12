4028796cc684493fff672bb447dde90957ac5a91
Kiara Hoheb <121005877+kxtara@users.noreply.github.com>
2023-10-12 00:57:39 -0400

    Added Task

--- ariels-app/src/assets/stuff/Todo.jsx
+++ ariels-app/src/assets/stuff/Todo.jsx
@@ -4,7 +4,6 @@
 // TODO:
 
 /*
-Todo: 8/17/23 (Completed)
     • Animation for navigation bar (it's alright)
     • Fix the background of the navigation (Checked)
     • Fix the hero text and button (Checked)
@@ -13,20 +12,21 @@ Todo: 8/17/23 (Completed)
     • Add screenshot to readme (Checked)
     • Convert css to Tailwind (Checked)
     • New Logo (Checked)
-
-Todo: 8/23/23
     • Create slider (Checked)
     • Set up links for slider elements (Checked)
     • Create a button to change back from the book button (Checked)
     • Create array of objects to store info about styles (Checked)
     • Get images and descritions for cards (Checked)
-
-Todo: 8/28/23
     • Write Process on Services page (Checked)
-    
-Todo: 9/29/23 
     • Continue Contact page (Checked)
-Todo: 10/2/23
     •  Work on desktop version
     • Work on popup when form is submitted (Checked)
+    
+    • Work on excluding dates/times on calendar after a user clicks pay (most likely exclude date/time after user pays - disable while user is on payment page, if user doesn't pay after a certain time enable date/time)
+    
+    • Figure out how to customize the calendar
+    • Work on styling for the booking page
+   //* • Research payment library (stripe)
+   • When a user navigates to another page close the navigation if left open
+   • After a user that doesn't require a deposit confirms their booking direct them to the home page (maybe)
 */
