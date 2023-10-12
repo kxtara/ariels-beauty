dd9ddab70e046af91d4f0df14b80042536491370
Kiara Hoheb <121005877+kxtara@users.noreply.github.com>
2023-10-11 22:13:06 -0400

    structuring booking page

--- ariels-app/src/pages/Booking.jsx
+++ ariels-app/src/pages/Booking.jsx
@@ -1,30 +1,58 @@
+// Import the necessary dependencies.
 import { useSearchParams } from "react-router-dom";
 import Hero from "../components/Hero";
+import Calendar from "../components/Calendar";
+
+// Define the Booking component.
 export default function Booking() {
+  // Retrieve search parameters from the URL.
   const [searchParams] = useSearchParams();
 
-  const { title, price, image, imageAlt } = Object.fromEntries(
+  // Extract data from the search parameters.
+  const { title, price, image, imageAlt} = Object.fromEntries(
     searchParams.entries()
   );
 
+  // Calculate the deposit amount based on the price.
+  const deposit = () => {
+    if (typeof price !== "number") {
+      let regex = /[0-9]/g;
+      let match = parseInt(price.match(regex).join(""));
+      return Math.floor(match * 0.25) < 15
+        ? ""
+        : `Deposit: $${Math.floor(match * 0.25)}`;
+    }
+  };
+
+  // Terms
+  const terms = () => {
+    return title !== 'Wash' ? 'Please arrive on time with clean,washed hair for the best results. We understand that unexpected delays can occur, so there is a 15-minute grace period for late arrivals.' : 'We understand that unexpected delays can occur, so there is a 15-minute grace period for late arrivals.'
+  }
+
+  // Render the Booking component.
   return (
     <>
-      <Hero title="hello" />
-      <div className="bg-[#fffdfd] border-[1px] border-[rgba(30, 30, 30, 43%)] rounded-[0.75rem] w-[24rem] m-auto mt-7 mb-7 flex">
-        <img
-          className="w-[9rem] rounded-tl-[0.3125rem] rounded-bl-[0.3125rem]"
-          src={image}
-          alt={imageAlt}
-        />
-        <div className="ml-3">
-          <h4 className="mt-4 font-bold uppercase">{title}</h4>
-          <h3 className="font-semibold ">{price}</h3>
-          <p>
-            <span className="font-semibold">Terms:</span>
-            blag lah mdidm
+      {/* Display a Hero component with a title */}
+      <Hero
+        title="Celebrating the beauty of women, one strand at a time."
+        className='absolute top-[45%] right-8 font-bold text-right w-[15rem] text-[#f8f8f8d9] font-["Kameron"] text-xl'
+      />
+      <div className="flex flex-col w-[15rem] m-auto mt-8">
+        {/* Display the title */}
+        <h2 className="font-semibold uppercase text-lg text-center">{title}</h2>
+        {/* Display the product image */}
+        <img className="w-[100%] mt-5" src={image} alt={imageAlt} />
+          <h3 className="mt-4">
+            {/* Display the deposit amount */}
+            <span className="font-semibold">{deposit()}</span>
+          </h3>
+          <p className="font-thin text-xs mt-2">
+            {/* Display "Terms:" */}
+            <span className="font-semibold mt-2 pr-2">Terms:</span>
+            {terms()}
           </p>
-        </div>
       </div>
+      <Calendar deposit={deposit()}/>
     </>
   );
 }
