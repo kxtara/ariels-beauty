2fb73485e2ebaa1068c2b316418fa6495f953757
Kiara Hoheb <121005877+kxtara@users.noreply.github.com>
2023-10-13 21:31:08 -0400

    Removed span tag from h1 tag

--- ariels-app/src/components/Header.jsx
+++ ariels-app/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Navigation from "./Navigation";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { Link } from "react-router-dom";
 const Header = () => {
   // Sets the variable menuIsVisible to a false state
   const [menuIsVisible, setMenuIsVisible] = useState(false);
@@ -10,20 +11,25 @@ const Header = () => {
   return (
       <header className="fixed w-full -mt-[.4rem] pb-[.6rem] z-[1]">
         <h1 className="bg-[#f8f8f8] pt-[.7rem] pr-0 pb-[.5rem] pl-[.7rem] font-['Lily_Script_One'] text-[1.5625rem]">
+        <Link
+        to='/'
+        >
           Ariel's Beauty
-          {/* Toggles visibility between hamburger icon and X icon */}
+        </Link>
+          
+        </h1>
+        {/* Toggles visibility between hamburger icon and X icon */}
           <span
-            className="mr-6 mt-1 float-right text-[#6c705f]"
+            className="mr-6 -mt-10 float-right text-[#828b67]"
             onClick={toggleVisibility}
           >
             {/* Displays the hamburger icon if menuIsVisibile is true otherwise X icon */}
             {!menuIsVisible ? (
-              <i className="bi bi-list w-8 cursor-pointer"></i>
+              <i className="bi bi-list cursor-pointer text-2xl"></i>
             ) : (
-              <i className="bi bi-x-lg w-8 cursor-pointer"></i>
+              <i className="bi bi-x-lg w-[10rem] cursor-pointer text-2xl"></i>
             )}
           </span>
-        </h1>
         {/* Hamburger Menu */}
         <Navigation toggleVisibility={toggleVisibility} menuIsVisible={menuIsVisible}/>
       </header>
