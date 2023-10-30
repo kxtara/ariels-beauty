456792df3f6671a72a3dd19ab3a64fba1b078d24
Kiara Hoheb <121005877+kxtara@users.noreply.github.com>
2023-10-29 20:51:28 -0400

    Filtered out items

--- ariels-app/src/components/Slider.jsx
+++ ariels-app/src/components/Slider.jsx
@@ -4,31 +4,35 @@ import "@splidejs/splide/dist/css/themes/splide-default.min.css";
 export default function SplideSlider({ items, onItemClick }) {
   // Define Splide options for the slider
   const splideOptions = {
-    perPage: 4,
-    focus: 0,
-    omitEnd: true,
+    perPage: 4,         // Number of slides displayed per view
+    focus: 0,           // Index of the initial focused slide
+    omitEnd: true,      // Omit partially visible slides at the end
   };
+
+  // Filter items to exclude those where the first element of the 'service' array is falsy
+  const filteredItems = items.filter((item) => item.service[0]);
 
   return (
     // Create a Splide slider with specified options
     <Splide options={splideOptions} className="flex z-0">
-      {items.map((item, index) => (
+      {filteredItems.map((item, index) => {
         // Create a slide for each item in the 'items' array
-        <SplideSlide key={index}>
-          <div onClick={() => onItemClick(item.service[1], item.service[2])}>
-            {/* Display an image with a click event */}
-            <img
-              className="bg-[#6C705F] rounded-[6rem] w-[4rem] h-[4rem] p-3 ml-4 cursor-grab"
-              src={item.service[0]}
-              alt={`${item.service[1]} logo`}
-            />
-            {/* Display the service name */}
-            <p className="font-bold font-['Katibeh'] ml-[1.76rem]">
-              {item.service[1]}
-            </p>
-          </div>
-        </SplideSlide>
-      ))}
+        return (
+          <SplideSlide key={index}>
+            <div onClick={() => onItemClick(item.service[1])}>
+            {/* display a logo with the name of the service */}
+              <img
+                className="bg-[#6C705F] rounded-[6rem] w-[4rem] h-[4rem] p-3 ml-4 cursor-grab"
+                src={item.service[0]}
+                alt={`${item.service[1]} logo`}
+              />
+              <p className="font-bold font-['Katibeh'] ml-[1.76rem]">
+                {item.service[1]}
+              </p>
+            </div>
+          </SplideSlide>
+        );
+      })}
     </Splide>
   );
 }
