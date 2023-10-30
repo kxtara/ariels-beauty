b55a7b5ac95fe8b7b799861927e3c57bc264a161
Kiara Hoheb <121005877+kxtara@users.noreply.github.com>
2023-10-29 20:52:41 -0400

    Moved file out of folder

---
+++ ariels-app/src/components/ServiceCards.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Card from "./Card";
+import { useData } from "../data/DataProvider";
+
+function ServiceCards({ service, showMore, setShowMore }) {
+  const data = useData()
+  // console.log('service :',service)
+  if(!data) return null
+  return (
+    <>
+      {data.map((items) => {
+        return items.collectionName === service
+          ? items.data.map((item) => {
+              const {
+                alt,
+                description,
+                detailedDescription,
+                id,
+                imageUrl,
+                price,
+                title,
+              } = item;
+              return (
+                <Card
+                  title={title}
+                  imageAlt={alt}
+                  key={id}
+                  image={imageUrl}
+                  description={description}
+                  detailedDescription={detailedDescription}
+                  price={price}
+                  showMore={showMore}
+                  setShowMore={setShowMore}
+                />
+              );
+            })
+          : null;
+      })}
+    </>
+  );
+}
+
+export default ServiceCards;
+
+/*
+braid.png
+
+hair-washing (2).png
+
+wig.png
+
+woman.png
+
+*/
