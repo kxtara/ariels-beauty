fe9c88f924bb864d45d62d1accde302c5ed06a69
Kiara Hoheb <121005877+kxtara@users.noreply.github.com>
2023-10-29 20:39:58 -0400

    Changed mock data to data from database

--- ariels-app/src/pages/Services.jsx
+++ ariels-app/src/pages/Services.jsx
@@ -1,23 +1,31 @@
 import React, { useState } from "react";
 import Hero from "../components/Hero";
 import Subtitle from "../components/Subtitle";
-import ServiceCards from "../components/ServiceCards/ServiceCards";
-import { featured, slideItems } from "../components/ServiceCards/ServicesInfo";
+import ServiceCards from "../components/ServiceCards";
 import SplideSlider from "../components/Slider";
+import { useData } from "../data/DataProvider";
 
 function Services() {
+  // State for managing the title and selected service
   const [title, setTitle] = useState("Popular Services");
-  const [service, setService] = useState(featured);
+  const [service, setService] = useState("Featured");
 
-  // This will change the display when you click more/X button
+  // State for controlling the display of additional content
   const [showMore, setShowMore] = useState(false);
 
-  // This will change the card details and subtitle based on the service clicked
-  const handleSliderItem = (itemTitle, itemService) => {
-    setTitle(itemTitle);
-    setService(itemService);
+  // Fetch data from 'DataProvider'
+  const data = useData();
 
-    // when a sliderItem is click the card display is set back to its default
+  // Initialize an array for slide data
+  const slide = data.map((item) => ({
+    service: [item.data[0].logoUrl, item.collectionName, item.data],
+  }));
+
+  // Handle the click event on the slider item
+  const handleSliderItem = (itemService) => {
+    setTitle(itemService);
+    setService(itemService);
+    // Reset the showMore state when a slider item is clicked
     setShowMore(false);
   };
 
@@ -28,11 +36,9 @@ function Services() {
         className="text-3xl text-center top-1/2 left-16"
       />
       <section className="py-2 -mb-3 mt-3">
-        {/* items takes in an array of objects */}
-        <SplideSlider items={slideItems} onItemClick={handleSliderItem} />
+        <SplideSlider items={slide} onItemClick={handleSliderItem} />
       </section>
       <Subtitle title={title} />
-      {/* Passing showMore and setShowMore to ServiceCards, which will then pass them to the Card component */}
       <ServiceCards
         service={service}
         showMore={showMore}
