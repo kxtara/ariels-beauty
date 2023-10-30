f10284948416a7ab358f829480ef0aaa6ae74200
Kiara Hoheb <121005877+kxtara@users.noreply.github.com>
2023-10-29 20:53:42 -0400

    Added tasks

--- ariels-app/src/assets/stuff/Todo.jsx
+++ ariels-app/src/assets/stuff/Todo.jsx
@@ -1,32 +1,27 @@
-// * Important
-// ! alert
-// ? question
-// TODO:
+// note = completed
 
-/*
-    • Animation for navigation bar (it's alright)
-    • Fix the background of the navigation (Checked)
-    • Fix the hero text and button (Checked)
-    • Add some padding to the footer (Checked)
-    • Fix the card components border and button (Checked)
-    • Add screenshot to readme (Checked)
-    • Convert css to Tailwind (Checked)
-    • New Logo (Checked)
-    • Create slider (Checked)
-    • Set up links for slider elements (Checked)
-    • Create a button to change back from the book button (Checked)
-    • Create array of objects to store info about styles (Checked)
-    • Get images and descritions for cards (Checked)
-    • Write Process on Services page (Checked)
-    • Continue Contact page (Checked)
-    •  Work on desktop version
-    • Work on popup when form is submitted (Checked)
-    
-    • Work on excluding dates/times on calendar after a user clicks pay (most likely exclude date/time after user pays - disable while user is on payment page, if user doesn't pay after a certain time enable date/time)
-    
-    • Figure out how to customize the calendar (Checked)
-Todo   • Work on styling for the booking page
-   //* • Research payment library (stripe)
-   • When a user navigates to another page close the navigation if left open
-   • After a user that doesn't require a deposit confirms their booking direct them to the home page (maybe) (Checked - popup)
-*/
+// TODO -    Research payment library (stripe)
+// TODO -    Work on excluding dates/times on calendar after a user clicks pay (most likely exclude date/time after user pays - disable while user is on payment page, if user doesn't pay after a certain time enable date/time)
+// TODO -    Work on desktop version
+// TODO -    Work on booking page design
+// REVIEW -  Animation for navigation bar (it's alright)
+// REVIEW -  When a user navigates to another page close the navigation if left open After a user that doesn't require a deposit confirms their booking direct them to the home page (maybe) (Checked - popup)
+
+
+// NOTE -    Fix the background of the navigation 
+// NOTE -    Fix the hero text and button 
+// NOTE -    Add some padding to the footer 
+// NOTE -    Fix the card components border and button 
+// NOTE -    Add screenshot to readme 
+// NOTE -    Convert css to Tailwind 
+// NOTE -    New Logo 
+// NOTE -    Create slider 
+// NOTE -    Set up links for slider elements 
+// NOTE -    Create a button to change back from the book button 
+// NOTE -    Create array of objects to store info about styles 
+// NOTE -    Get images and descritions for cards 
+// NOTE -    Write Process on Services page 
+// NOTE -    Continue Contact page 
+// NOTE -    Work on popup when form is submitted 
+// NOTE -    Figure out how to customize the calendar 
+// NOTE      Work on styling for the booking page
