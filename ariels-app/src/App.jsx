d7d54a4fa5fdb496a2497ea1b39208f86c989f85
Kiara Hoheb <121005877+kxtara@users.noreply.github.com>
2023-10-29 20:47:36 -0400

    Added 'DataProvider' to pass data to components

--- ariels-app/src/App.jsx
+++ ariels-app/src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import AppRouter from "./routes/AppRouter";
+import { DataProvider } from "./data/DataProvider";
 import './index.css'
 function App() {
   return (
     <>
+    <DataProvider>
       <AppRouter />
+    </DataProvider>
     </>
   );
 }
