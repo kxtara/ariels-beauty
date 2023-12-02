f0e2fe1c87a389b7719a610e81787a3fe6cc0f37
Kiara Hoheb <121005877+kxtara@users.noreply.github.com>
2023-12-02 10:08:41 -0400

    Edited task

--- ariels-app/src/assets/stuff/Todo.jsx
+++ ariels-app/src/assets/stuff/Todo.jsx
@@ -1,27 +1,40 @@
 // note = completed
 
 // TODO -  Research payment library (stripe)
-// TODO -  Work on excluding times on calendar after a user clicks pay (most likely exclude time after user pays - disable while user is on payment page, if user doesn't pay after a certain time enable time)
-// TODO - Keep selected time as excluded temporarily until user pays,after 30min, or page refresh/exit
+
+/* TODO -  
+
+• Work on excluding times on calendar after a user clicks pay (most likely exclude time after user pays - disable while user is on payment page, if user doesn't pay after a certain time enable time)
+
+- We're going to try using firebase since we've already used it in this project.
+
+- We'll store excluded times in firebase, that time being the time the current user click.
+
+- Have to figure out how the calendar for other users will be updated when a user selects a time
+*/
+
+
 // TODO -    Work on desktop version
 // TODO -    Work on booking page design
+
 // REVIEW -  Animation for navigation bar (it's alright)
 // REVIEW -  When a user navigates to another page close the navigation if left open After a user that doesn't require a deposit confirms their booking direct them to the home page (maybe) (Checked - popup)
 
-// NOTE -    Fix the background of the navigation
-// NOTE -    Fix the hero text and button
-// NOTE -    Add some padding to the footer
-// NOTE -    Fix the card components border and button
-// NOTE -    Add screenshot to readme
-// NOTE -    Convert css to Tailwind
-// NOTE -    New Logo
-// NOTE -    Create slider
-// NOTE -    Set up links for slider elements
-// NOTE -    Create a button to change back from the book button
-// NOTE -    Create array of objects to store info about styles
-// NOTE -    Get images and descritions for cards
-// NOTE -    Write Process on Services page
-// NOTE -    Continue Contact page
-// NOTE -    Work on popup when form is submitted
-// NOTE -    Figure out how to customize the calendar
-// NOTE      Work on styling for the booking page
+// <--- COMPLETED --->
+//    Fix the background of the navigation
+//    Fix the hero text and button
+//    Add some padding to the footer
+//    Fix the card components border and button
+//    Add screenshot to readme
+//    Convert css to Tailwind
+//    New Logo
+//    Create slider
+//    Set up links for slider elements
+//    Create a button to change back from the book button
+//    Create array of objects to store info about styles
+//    Get images and descritions for cards
+//    Write Process on Services page
+//    Continue Contact page
+//    Work on popup when form is submitted
+//    Figure out how to customize the calendar
+//    Work on styling for the booking page
