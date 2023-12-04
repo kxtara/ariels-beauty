9f9fb34aadf3419bdd90036b4eed975931be32a4
Kiara Hoheb <121005877+kxtara@users.noreply.github.com>
2023-12-04 16:41:23 -0400

    Removed DataProvide

--- ariels-app/src/App.jsx
+++ ariels-app/src/App.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import AppRouter from "./routes/AppRouter";
-import { DataProvider } from "./data/DataProvider";
-import './index.css'
+import "./index.css";
 function App() {
   return (
     <>
-    <DataProvider>
-      <AppRouter />
-    </DataProvider>
+          <AppRouter />
     </>
   );
 }
