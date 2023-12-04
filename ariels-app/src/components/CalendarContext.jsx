ee895527af1721b09efe68007f72881610382466
Kiara Hoheb <121005877+kxtara@users.noreply.github.com>
2023-12-04 16:43:17 -0400

    Used  context to pass data instead of passing it through the url

---
+++ ariels-app/src/components/CalendarContext.jsx
@@ -0,0 +1,19 @@
+import { useState,useContext,createContext } from "react";
+
+const CalendarContext = createContext()
+
+export const CalendarProvider = ({children}) => {
+  const [calendarData,setCalendarData] = useState({})
+
+  const setData = (deposit,formattedTime) => {setCalendarData({deposit,formattedTime})}
+  console.log("CalendarProvider - CalendarData:",calendarData)
+  return(
+    <CalendarContext.Provider value={{ calendarData, setData }}>
+      {children}
+    </CalendarContext.Provider>
+  )
+}
+
+export const useCalendarContext = () => {
+  return useContext(CalendarContext)
+}
